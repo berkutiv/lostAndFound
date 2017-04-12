065925ab0017feec2d56ef05f260cc234673a546
berkutiv <berkutiv@MacBook-Pro-Ivan.local>
2017-04-12 18:45:55 +0300

    поменял название

--- LostAndFound/MapViewController.swift
+++ LostAndFound/MapViewController.swift
@@ -8,19 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         print("ПРИВЕТ МАКС")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
