38b1d5fc8b8c5f1be78adb4bc04b9658c8c8603e
berkutiv <berkutiv@MacBook-Pro-Ivan.local>
2017-04-14 14:17:24 +0300

    Кастомные ячейки

--- LostAndFound/MapViewController.swift
+++ LostAndFound/MapViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController
 {
@@ -17,6 +18,22 @@ class MapViewController: UIViewController
         // Do any additional setup after loading the view, typically from a nib.
         
         print("ПРИВЕТ МАКС")
+    }
+    
+    override func loadView() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 
 
---
+++ LostAndFound/UserHeaderTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserHeaderTableViewCell.swift
+//  LostAndFound
+//
+//  Created by Иван on 14.04.17.
+//  Copyright © 2017 Иван. All rights reserved.
+//
+
+import UIKit
+
+class UserHeaderTableViewCell: UITableViewCell
+{
+
+    
+}
